91fda71d9ac07442c05170216ad6a9de48c239e2
Sandor Irlanda <sandor.irlanda@gmail.com>
2014-12-04 19:03:19 +0100

    mock with EasyMock

--- src/test/java/hu/tigra/tutorial/ejb/UserStatFacadeTest.java
+++ src/test/java/hu/tigra/tutorial/ejb/UserStatFacadeTest.java
@@ -1,20 +1,43 @@
 package hu.tigra.tutorial.ejb;
 
+//import static org.easymock.EasyMock.*;                 // when mocking interfaces
+import static org.easymock.classextension.EasyMock.*;    // when mocking classes without interface
+
 import hu.tigra.tutorial.dao.UserDAO;
+import hu.tigra.tutorial.dmo.User;
+import org.easymock.classextension.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class UserStatFacadeTest {
 
+    UserStatFacade facade;
+    UserDAO mockDao;
+
+    @Before
+    public void setUp() throws Exception {
+        facade = new UserStatFacade();
+        mockDao = EasyMock.createMock(UserDAO.class);
+        facade.userDAO = mockDao;
+    }
+
     @Test
     public void testCalculateAgeDistributionByPostcode() throws Exception {
-        UserStatFacade facade = new UserStatFacade();
-        facade.userDAO = new UserDAO();
+        final String postcode = "1111";
+        final User user = new User();
+        user.setDateOfBirth(new Date());
 
-        final Map<String, Long> ageDistributionByPostcode = facade.calculateAgeDistributionByPostcode("1111");
+        expect(mockDao.getUsersByPostCode(postcode)).andReturn(Arrays.asList(user));
+
+        replay(mockDao);
+
+        final Map<String, Long> ageDistributionByPostcode = facade.calculateAgeDistributionByPostcode(postcode);
 
         assertEquals(5, ageDistributionByPostcode.size());
         assertEquals(1l, ageDistributionByPostcode.get(UserStatFacade.AGE_GROUP_KIDS).longValue());
